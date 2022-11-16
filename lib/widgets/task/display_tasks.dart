850976baab38201164c28b4d230c542ca431f2bb
mritter <mritter@ci-database.de>
2022-11-16 23:20:16 +0100

    tests

--- lib/widgets/task/display_tasks.dart
+++ lib/widgets/task/display_tasks.dart
@@ -15,8 +15,98 @@ class DispayTasks extends StatelessWidget {
     var taskmodel = context.watch<TaskModel>();
     var taskcontroller = context.watch<TaskController>();
 
+    double height(candidateData) {
+      if (candidateData.isNotEmpty) {
+        return 20.0;
+      }
+      return 10;
+    }
+
     final taskfolders = taskmodel.taskfolders;
+    return Container(
+      //  color: Colors.amber,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListView.builder(
+              shrinkWrap: true,
+              itemCount: taskfolders.length,
+              itemBuilder: (BuildContext context, int idxFolder) {
+                return Column(
+                  children: [
+                    Text("Header1"),
+                    ListView.builder(
+                        itemCount: taskfolders[idxFolder].tasks.length,
+                        physics: ClampingScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: (BuildContext context, int idxTask) {
+                          return Column(
+                            children: [
+                              DragTarget(
+                                onMove: (details) {
+                                  print("move");
+                                  print(details);
+                                },
+                                onAcceptWithDetails: (details) {
+                                  print(details);
+                                },
+                                builder:
+                                    (context, candidateData, rejectedData) {
+                                  return Container(
+                                    color: (candidateData.isNotEmpty)
+                                        ? Colors.grey
+                                        : null,
+                                    height: height(candidateData),
+                                  );
+                                },
+                                onAccept: (data) {
+                                  print(data);
+                                },
+                              ),
+                              Draggable(
+                                data: idxTask,
+                                feedback: Text(taskfolders[idxFolder]
+                                    .tasks[idxTask]
+                                    .title),
+                                child: Container(
+                                  color: Colors.black12,
+                                  child: Text(taskfolders[idxFolder]
+                                      .tasks[idxTask]
+                                      .title),
+                                ),
+                              ),
+                            ],
+                          );
+                        })
+                  ],
+                );
+                /*
+
+                            ListView.builder(
+                        itemCount: taskfolders[idxFolder].tasks.length,
+                        physics: ClampingScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: (BuildContext context, int idxTask) {
+                          return Text(
+                              taskfolders[idxFolder].tasks[idxTask].title);
+                        })
+
+
+                return ListView.builder(
+                    itemCount: taskfolders.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Text('Child');
+                    });
+                    */
+              }),
+        ],
+      ),
+    );
+    /*
     final contents = List.generate(taskfolders.length, (folderIdx) {
+
+
+
       final tasks = taskfolders[folderIdx].tasks;
       final children = List.generate(tasks.length, (taskIdx) {
         return DragAndDropItem(
@@ -81,34 +171,6 @@ class DispayTasks extends StatelessWidget {
                             )*/
                             ))
 
-                    /*
-                        child: InkWell(
-                            onTap:
-                                (taskcontroller.action == TaskActionStatus.none)
-                                    ? () {
-                                        if (taskcontroller.action ==
-                                            TaskActionStatus.none) {
-                                          if (taskcontroller.selectedTaskId ==
-                                              tasks[taskIdx].id) {
-                                            taskmodel.openFormular(
-                                                TaskActionStatus.save);
-                                          } else {
-                                            taskcontroller.setSelectedID(
-                                                taskID: tasks[taskIdx].id,
-                                                folderIdx: folderIdx);
-                                          }
-                                        }
-                                      }
-                                    : null,
-                            child: Ink(
-                                color: (taskcontroller.selectedTaskId ==
-                                        tasks[taskIdx].id)
-                                    ? Theme.of(context).focusColor
-                                    : null,
-                                child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(tasks[taskIdx].title))))  )
-                                   */
                   ]),
                 ),
                 Visibility(
@@ -121,6 +183,7 @@ class DispayTasks extends StatelessWidget {
       });
 
       return DragAndDropList(children: children, canDrag: false);
+    
     });
 
     return Padding(
@@ -160,5 +223,6 @@ class DispayTasks extends StatelessWidget {
         ],
       ),
     );
+      */
   }
 }
